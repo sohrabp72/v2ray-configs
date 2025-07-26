d6fd706f2fb511ed5f906b00ba0e762371adcdfc
ᛖᛈᛟᛞᛟᚾᛁᛟᛊ <epodonios@gmail.com>
2025-07-27 00:25:52 +0330

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -107,7 +107,6 @@ def main():
         "https://raw.githubusercontent.com/Everyday-VPN/Everyday-VPN/main/subscription/main.txt",
         "https://raw.githubusercontent.com/C4ssif3r/V2ray-sub/main/all.txt",
         "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
-        "https://github.com/Epodonios/v2ray-configs/raw/main/All_Configs_Sub.txt"
     ]
 
     decoded_links = decode_links(links)
